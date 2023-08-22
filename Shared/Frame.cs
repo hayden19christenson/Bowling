da30bbfe96cd083bac9f9b115b459f234d059efd
Hayden Christenson <93932020+HaydenChristenson@users.noreply.github.com>
2023-08-22 17:04:18 -0500

    Bowling Challenge

---
+++ Shared/Frame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingChallenge.Shared
+{
+    public class Frame
+    {
+        public bool isLast { get; set; }
+
+        public int bonusRolls { get; set; }
+
+        public int currentRoll { get; set; }
+
+        public int frameScore { get; set; }
+    }
+}
---
+++ Shared/Scoreboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingChallenge.Shared
+{
+    public class Scoreboard
+    {
+        public List<Frame> Frames;
+
+        public int Total;
+
+        public int currentFrame;
+
+        //Initialized Scoreboard to have its last frame have a possible 3rd roll
+        public Scoreboard()
+        {
+            List<Frame> fullFrames = new List<Frame>();
+
+            for(int i = 0; i < 9; i++)
+            {
+                fullFrames.Add(new Frame());
+            }
+
+            Frame lastFrame = new Frame();
+            lastFrame.isLast = true;
+            fullFrames.Add(lastFrame);
+
+            Frames = fullFrames;
+        }
+    }
+
+}
